0dbe66c38ca80d1f86df911b2b0aab56613793c8
Bessan20 <a100baisanmohamed@gmail.com>
2025-02-19 13:46:56 +0200

    Add files via upload

---
+++ constructor exercises/exe1/main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+/*
+1. Default Constructor:
+Write a c++ program to create a class called "Cat" with instance variables name and age. 
+Implement a default constructor that initializes the name to "Unknown" and the age to 0. 
+Print the values of the variables.
+*/
+
+class Cat {
+
+  private :
+  int name;
+  int age;
+  
+  public :
+  Cat() {
+    
+  }
+};
+int main()
+{
+
+
+}

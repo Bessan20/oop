698769cad3ac715d030fc64c660b3aeb03b1968e
abc <abc@example.com>
2025-02-19 15:12:18 +0200

    Constructor with Default Values

---
+++ constructor exe/exe4/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Car {
+
+    public :
+    string model;
+    int year;
+
+    Car(string model = "Kia" , int year = 2014) {
+        this->model = model;
+        this->year = year;
+        cout << "Car constructor called with model: " << model << ", year: " << year << endl;
+    }
+};
+
+int main()
+{
+
+    Car car1;
+    Car car2("Ford" , 2019);
+    return 0;
+
+}
